0d4069c3477a3d7483a36552c46dce4b2c341da8
Jose Angel de Bustos Perez <jadebustos@gmail.com>
2022-04-11 20:46:11 +0200

    dataset creation

--- create-dataset/entrypoint.sh
+++ create-dataset/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-python3 create-dataset.py --device $VIDEO_INDEX --modulus $MODULUS --range $RANDOM_RANGE --size $IMAGE_SIZE --output_dir $OUTPUT_DIR
+python3 create-dataset.py --device $VIDEO_INDEX --modulus $MODULUS --range $RANDOM_RANGE \
+                          --width $WIDTH --height $HEIGHT \ 
+                          --size $IMAGE_SIZE --output_dir $OUTPUT_DIR
