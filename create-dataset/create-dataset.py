b4867571c6e2d91a93ae5081bd8f3d30ce829b9a
Jose Angel de Bustos Perez <jadebustos@gmail.com>
2022-04-11 13:46:06 +0200

    dataset creation

--- create-dataset/create-dataset.py
+++ create-dataset/create-dataset.py
@@ -11,12 +11,26 @@ import cv2
 import os
 
 def face_detection(args):
-    output_dir = '/srv/video/output'
-    output_dir = '/home/jadebustos/working/demo-ai'
+    print(args)
     videoDev = int(args['device'])
     cascPath = 'haarcascade_frontalface_default.xml'
     faceCascade = cv2.CascadeClassifier(cascPath)
+    if args['output_dir'] == None:
+        output_dir = '/srv/video/output'
+    else:
+        output_dir = args['output_dir']
+    if args['modulus'] == None:
+        modulus = 7
+    else:
+        modulus = int(args['modulus'])
+    if args['range'] == None:
+        random_range = 101
+    else:
+        random_range = int(args['range'])
 
+    print(output_dir)
+    print(modulus)
+    print(random_range)
     # gauss dev=1 (internal)
     # ramanujan dev=2 (logitech)
     # archimedes dev=5 (logitech)
@@ -55,8 +69,8 @@ def face_detection(args):
                 # to avoid creating images which are too similar only
                 # some random frames will be stored
                 # (maquiavelo seal of approval)
-                random_int = random.randint(1,101)
-                if random_int % 7 == 0:
+                random_int = random.randint(1, random_range)
+                if random_int % modulus == 0:
                     # create random name
                     filename = "frame-"+ ''.join(random.choice(string.ascii_lowercase) for i in range(15))+ ".jpg"
                     filepath = os.path.join(output_dir, filename)
@@ -75,6 +89,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Just an example",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-d", "--device", type=int, required=True, help="video device index")
+    parser.add_argument("-m", "--modulus", type=int, help="modulus")
+    parser.add_argument("-r", "--range", type=int, help="random range")
+    parser.add_argument("-o", "--output_dir", help="output dir where store images")
     parser.add_argument("-g", "--graphical", action="store_true", help="graphical")
     args = parser.parse_args()
 
