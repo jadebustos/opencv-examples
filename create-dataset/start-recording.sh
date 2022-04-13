e0fa7115b25a71c63a57bb852144fb854f05bbcb
Jose Angel de Bustos Perez <jadebustos@gmail.com>
2022-04-13 15:58:55 +0200

    local directory to variable in script

--- create-dataset/start-recording.sh
+++ create-dataset/start-recording.sh
@@ -7,9 +7,10 @@ MY_RANDOM_RANGE=101
 MY_IMAGE_SIZE=400
 MY_WIDTH=1920
 MY_HEIGHT=1080
+LOCAL_DIRECTORY_STORE_IMAGES="/home/jadebustos/working/demo-ai/"
 
 podman run --rm --env VIDEO_INDEX=$MY_VIDEO_INDEX --env MODULUS=$MY_MODULUS \
            --env RANDOM_RANGE=$MY_RANDOM_RANGE --env OUTPUT_DIR=/srv/video/output \
            --env IMAGE_SIZE=$MY_IMAGE_SIZE --env WIDTH=$MY_WIDTH --env HEIGHT=$MY_HEIGHT \
            --privileged -v /dev/:/dev:rslave --mount type=devpts,destination=/dev/pts \
-           -v /home/jadebustos/working/demo-ai/:/srv/video/output:Z -d quay.io/rhte_2019/face-dataset:v2
+           -v $LOCAL_DIRECTORY_STORE_IMAGES:/srv/video/output:Z -d quay.io/rhte_2019/face-dataset:v2
