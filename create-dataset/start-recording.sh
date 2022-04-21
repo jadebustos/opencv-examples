11890308111fcd99b138eb37c49159fbf7a82398
Jose Angel de Bustos Perez <jadebustos@gmail.com>
2022-04-21 15:37:46 +0200

    dataset-creation script, image to var

--- create-dataset/start-recording.sh
+++ create-dataset/start-recording.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # configure according to your environment
-MY_VIDEO_INDEX=1
+MY_VIDEO_INDEX=5
 MY_MODULUS=17
 MY_RANDOM_RANGE=101
 MY_IMAGE_SIZE=400
 MY_WIDTH=1920
 MY_HEIGHT=1080
 LOCAL_DIRECTORY_STORE_IMAGES="/home/jadebustos/working/demo-ai/"
+IMAGE="quay.io/rhte_2019/face-dataset:v2"
 
 podman run --rm --env VIDEO_INDEX=$MY_VIDEO_INDEX --env MODULUS=$MY_MODULUS \
            --env RANDOM_RANGE=$MY_RANDOM_RANGE --env OUTPUT_DIR=/srv/video/output \
            --env IMAGE_SIZE=$MY_IMAGE_SIZE --env WIDTH=$MY_WIDTH --env HEIGHT=$MY_HEIGHT \
            --privileged -v /dev/:/dev:rslave --mount type=devpts,destination=/dev/pts \
-           -v $LOCAL_DIRECTORY_STORE_IMAGES:/srv/video/output:Z -d quay.io/rhte_2019/face-dataset:v2
+           -v $LOCAL_DIRECTORY_STORE_IMAGES:/srv/video/output:Z -d $IMAGE
